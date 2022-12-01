95157eb8d884ecc6df4001203e7471e5b78d6da3
Andrew Lauck <andrewlauck@andrews-air.wifi.uc.edu>
2022-12-01 13:22:45 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int age = 22;
+
+       if (age>= 21)
+       {
+           System.out.println("You get  wrist band");
+       }
+       else
+       {
+           System.out.println("You get no wrist band");
+       }
+    }
+}
